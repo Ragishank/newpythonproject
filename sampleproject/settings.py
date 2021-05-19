037dbfb1c4eaee41d193c3ac80cb49848bc64e2f
ragishabaabte <ragisha@baabte.com>
2021-05-19 18:41:41 +0530

    v3

--- sampleproject/settings.py
+++ sampleproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-z+pri9g#y^f9etn+vv2dvd+hwb#qwuny6v9+-e^rh@c&e_wqh9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
